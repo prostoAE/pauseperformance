c32dcc2c9b571ee011765b2e00a27dad18eb6d0b
prostoAE-nt <trident998@gmail.com>
2022-06-09 11:18:30 +0200

    some views fixes

---
+++ resources/views/app/includes/sidebar_menu.blade.php
@@ -0,0 +1,69 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="../widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-home"></i>
+                <p>Home</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="../widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-folder"></i>
+                <p>Activities</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="../widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
+                <p>
+                    Settings
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="../mailbox/mailbox.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>User Profile</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/compose.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Roles and Permissions</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/read-mail.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Company Information</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/read-mail.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Brand and Product</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/read-mail.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Campaign</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/read-mail.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Lanquage</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../mailbox/read-mail.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Payments</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
---
+++ resources/views/app/parts/header.blade.php
@@ -0,0 +1,55 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <form action="#">
+        <div class="row">
+            <div class="col-sm-6">
+                <select class="form-control select2 ml-3">
+                    <option selected="selected">Organisation 1</option>
+                    <option>Organisation 2</option>
+                    <option>Organisation 3</option>
+                    <option>Organisation 4</option>
+                </select>
+            </div>
+            <div class="col-sm-6">
+                <button type="button" class="btn btn-block btn-outline-primary ml-3">Add New</button>
+            </div>
+        </div>
+    </form>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->
+        <li class="nav-item">
+            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                <i class="fas fa-search"></i>
+            </a>
+            <div class="navbar-search-block">
+                <form class="form-inline">
+                    <div class="input-group input-group-sm">
+                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-navbar" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
